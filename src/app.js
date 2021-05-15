9eadab815fbc7326342ada640bbf4bc1226ce433
NikoHamburg <nikotrettin@googlemail.com>
2021-05-15 12:47:34 +0200

    add new menu

--- src/app.js
+++ src/app.js
@@ -1,37 +1,66 @@
-import './reset.css';
-import './app.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "./reset.css";
+import "./app.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import Header from './components/header/header';
-import Main from './components/main/main';
-import About from './components/about/about';
-import Contact from './components/contact/contact';
-import Work from './components/work/work';
-import Footer from './components/footer/footer';
+import Header from "./components/header/header";
+import Main from "./components/main/main";
+import About from "./components/about/about";
+import Contact from "./components/contact/contact";
+import Work from "./components/work/work";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <Router>
-    <div>
       <Header />
       <div className="app">
-      <Switch>
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/contact'>
-          <Contact />
-        </Route>
-        <Route path='/work'>
-          <Work />
-        </Route>
-        <Route path='/'>
-          <Main />
-        </Route>
-      </Switch>
-      </div>      
+        <aside>
+          <div className="menu">
+            <div className="wrapper">
+              <ul>
+                <li>
+                  <Link className="nav--link" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav--link" to="/about">
+                    Information
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav--link" to="/work">
+                    Work
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav--link" to="/contact">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </aside>
+        <div className="inhalt">
+          <Switch>
+            <Route path="/">
+              <Main />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/work">
+              <Work />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
+      </div>
       <Footer />
-    </div>
     </Router>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,4 @@
 import "./header.css";
-import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -8,24 +7,16 @@ function Header() {
       <div className="title">nikoHamburg</div>
       <ul>
         <li>
-          <Link className="nav--link" to="/">
-            Home
-          </Link>
+
         </li>
         <li>
-          <Link className="nav--link" to="/about">
-            About
-          </Link>
+
         </li>
         <li>
-          <Link className="nav--link" to="/work">
-            Work
-          </Link>
+
         </li>
         <li>
-          <Link className="nav--link" to="/contact">
-            Contact
-          </Link>
+
         </li>
       </ul>
     </nav>
